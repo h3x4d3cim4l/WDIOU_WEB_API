bf2296823e6f6e8810a6f1f5ecf943658e6754cd
h3x4d3cim4l <przeglowa@gmail.com>
2023-11-29 22:48:24 +0100

    Dodaj pliki projektów.

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace WDIOU_WEB_API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ Models/WDIOUDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WDIOU_WEB_API.Models
+{
+    public class WDIOUDbContext : DbContext
+    {
+        public WDIOUDbContext( DbContextOptions<WDIOUDbContext> options ) : base(options)
+        {
+             
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}

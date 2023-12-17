c93b236d9549b1d1d315c5129ac151248fd7e962
h3x4d3cim4l <przeglowa@gmail.com>
2023-12-17 20:47:13 +0100

    API settings

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using WDIOU_WEB_API.Models;
+using WDIOU_WEB_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<WDIOUDatabaseSettings>(builder.Configuration.GetSection("WDIOUDatabase"));
+
+builder.Services.AddSingleton<UsersService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<WDIOUDbContext>(opt => opt.UseInMemoryDatabase("WDIOU"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 

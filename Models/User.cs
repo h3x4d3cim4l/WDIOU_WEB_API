bfedec83b33be6194cd0d66bd668f4e04499013b
h3x4d3cim4l <przeglowa@gmail.com>
2023-12-17 20:46:17 +0100

    API Models for User data/ database settings

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,12 @@
-﻿namespace WDIOU_WEB_API.Models
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace WDIOU_WEB_API.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string email { get; set; }
---
+++ Models/WDIOUDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WDIOU_WEB_API.Models
+{
+    public class WDIOUDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string UsersCollectionName { get; set; } = null!;
+    }
+}
